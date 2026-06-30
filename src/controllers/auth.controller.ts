ec3b82c381bade0a2754b3bea116d5df89ef393d
Pawan Nanhe <aniketnanhe64@gmail.com>
2026-06-30 21:30:53 +0530

    Tokens and Middleware Implemented

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,42 +1,54 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-console.log(process.env.JWT_SECRET);
+
 export const register = (req: Request, res: Response) => {
-
-    const token = jwt.sign(
-        {
-            id: 1,
-            role: "STUDENT"
-        },
-        process.env.JWT_SECRET as string,
-        {
-            expiresIn: "7d",
-        }
-    );
-
-    res.status(201).json({
-        success: true,
-        message: "User registered successfully",
-        token,
+  if (!process.env.JWT_SECRET) {
+    return res.status(500).json({
+      success: false,
+      message: "JWT_SECRET is missing",
     });
+  }
+
+  const token = jwt.sign(
+    {
+      id: 1,
+      role: "STUDENT",
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  res.status(201).json({
+    success: true,
+    message: "User registered successfully",
+    token,
+  });
 };
 
 export const login = (req: Request, res: Response) => {
-
-    const token = jwt.sign(
-        {
-            id: 1,
-            role: "STUDENT"
-        },
-        process.env.JWT_SECRET as string,
-        {
-            expiresIn: "7d",
-        }
-    );
-
-    res.status(200).json({
-        success: true,
-        message: "Login successful",
-        token,
+  if (!process.env.JWT_SECRET) {
+    return res.status(500).json({
+      success: false,
+      message: "JWT_SECRET is missing",
     });
+  }
+
+  const token = jwt.sign(
+    {
+      id: 1,
+      role: "STUDENT",
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Login successful",
+    token,
+  });
 };
